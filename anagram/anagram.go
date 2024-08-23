5ed33c4ba6c8dc34050d74ebe8c1fccae5386503
edgarsilva <edgar.osc@gmail.com>
2024-08-23 16:02:30 -0600

    Sorted version for anagram.

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -1,19 +1,16 @@
 package anagram
 
+import (
+	"sort"
+	"strings"
+)
+
 type empty struct{}
 
 func IsAnagram(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
-
-	// ss := strings.Split(s, "")
-	// sort.Strings(ss)
-	// sc := strings.Join(ss, "")
-	//
-	// ts := strings.Split(t, "")
-	// sort.Strings(ts)
-	// tc := strings.Join(ts, "")
 
 	freq := make(map[rune]int)
 
@@ -29,4 +26,30 @@ func IsAnagram(s string, t string) bool {
 	}
 
 	return true
+}
+
+func IsAnagramWithSort(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	// sa := strings.Split(s, "")
+	// ss := slices.SortedFunc(slices.Values(sa), func(m1, m2 string) int {
+	// 	return cmp.Compare(m1, m2)
+	// })
+	//
+	// ta := strings.Split(t, "")
+	// ts := slices.SortedFunc(slices.Values(ta), func(m1, m2 string) int {
+	// 	return cmp.Compare(m1, m2)
+	// })
+
+	ss := strings.Split(s, "")
+	sort.Strings(ss)
+	sc := strings.Join(ss, "")
+
+	ts := strings.Split(t, "")
+	sort.Strings(ts)
+	tc := strings.Join(ts, "")
+
+	return sc == tc
 }
