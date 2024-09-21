f5f5bf78af646344e6d14c96dfa407510aff22c8
edgarsilva <edgar.osc@gmail.com>
2024-09-21 17:09:25 -0600

    Refactor to connect4 to make win situation easier to understand
    And use arrays instead of slices.

--- connect4/main.go
+++ connect4/main.go
@@ -11,11 +11,43 @@ const (
 	cols = 7
 )
 
+type Column struct {
+	Rows [rows]string
+}
+
+func (c *Column) Length() int {
+	for i := 0; i < len(c.Rows); i++ {
+		if c.Rows[i] != "" {
+			return i + 1
+		}
+	}
+
+	return 0
+}
+
+func (c *Column) Push(val string) {
+	for i := 0; i < len(c.Rows); i++ {
+		if c.Rows[i] != "" {
+			continue
+		}
+
+		c.Rows[i] = val
+		break
+	}
+}
+
+type Board struct {
+	Columns [cols]Column
+}
+
+func (b *Board) Point(col, row int) string {
+	return b.Columns[col].Rows[row]
+}
+
 func main() {
-	board := [][]string{}
+	board := Board{}
 	for i := 0; i < cols; i++ {
-		col := make([]string, 0, cols)
-		board = append(board, col)
+		board.Columns[i] = Column{}
 	}
 
 	playerTurn := "A"
@@ -48,11 +80,10 @@ func main() {
 		}
 
 		inputCol--
-		col := board[inputCol]
-
-		if len(col) < rows {
-			col := append(col, playerTurn)
-			board[inputCol] = col
+		col := board.Columns[inputCol]
+		if col.Length() < rows {
+			col.Push(playerTurn)
+			board.Columns[inputCol] = col
 		} else {
 			fmt.Println("this column is already full", cols)
 		}
@@ -73,7 +104,7 @@ const PrevLine = "\033[F"
 
 var firstPaint = true
 
-func PrintBoard(board [][]string, playerTurn string) {
+func PrintBoard(board Board, playerTurn string) {
 	rewind := ""
 	if firstPaint {
 		firstPaint = false
@@ -86,35 +117,26 @@ func PrintBoard(board [][]string, playerTurn string) {
 
 	for i := rows - 1; i >= 0; i-- {
 		for j := 0; j < cols; j++ {
-			l := len(board[j])
-			if l > 0 && i < l {
-				fmt.Printf("%v ", board[j][i])
-			} else {
+			col := board.Columns[j]
+			if col.Rows[i] == "" {
 				fmt.Printf("- ")
+			} else {
+				fmt.Printf("%v ", col.Rows[i])
 			}
 		}
+
 		fmt.Printf("\n")
 	}
 }
 
-func checkColRows(col []string) bool {
-	rows := len(col)
-	return rows < 4
-}
-
-func CheckWin(board [][]string, player string) bool {
+func CheckWin(board Board, player string) bool {
 	// Check vertically
 	for col := 0; col < cols; col++ {
-		colRows := len(board[col])
-		if colRows < 4 {
-			continue
-		}
-
-		for row := 0; row <= colRows-4; row++ {
-			if board[col][row] == player &&
-				board[col][row+1] == player &&
-				board[col][row+2] == player &&
-				board[col][row+3] == player {
+		for row := 0; row <= rows-4; row++ {
+			if board.Point(col, row) == player &&
+				board.Point(col, row+1) == player &&
+				board.Point(col, row+2) == player &&
+				board.Point(col, row+3) == player {
 				return true
 			}
 		}
@@ -122,88 +144,39 @@ func CheckWin(board [][]string, player string) bool {
 
 	// Check horizontally
 	for col := 0; col <= cols-4; col++ {
-		col1Rows := len(board[col])
-		col2Rows := len(board[col+1])
-		col3Rows := len(board[col+2])
-		col4Rows := len(board[col+3])
-
-		if col1Rows <= 0 || col2Rows <= 0 || col3Rows <= 0 || col4Rows <= 0 {
-			continue
-		}
-
 		for row := 0; row < rows; row++ {
-			if row >= col1Rows || row >= col2Rows || row >= col3Rows || row >= col4Rows {
-				break
-			}
-
-			if board[col][row] == player &&
-				board[col+1][row] == player &&
-				board[col+2][row] == player &&
-				board[col+3][row] == player {
+			if board.Point(col, row) == player &&
+				board.Point(col+1, row) == player &&
+				board.Point(col+2, row) == player &&
+				board.Point(col+3, row) == player {
 				return true
 			}
 		}
 	}
 
+	// Check upslope diagonal
 	for col := 0; col <= cols-4; col++ {
-		col1Rows := len(board[col])
-		col2Rows := len(board[col+1])
-		col3Rows := len(board[col+2])
-		col4Rows := len(board[col+3])
-
-		if col1Rows <= 0 || col2Rows <= 0 || col3Rows <= 0 || col4Rows <= 0 {
-			continue
-		}
-
 		for row := 0; row <= rows-4; row++ {
-			if row >= col1Rows || row+1 >= col2Rows || row+2 >= col3Rows || row+3 >= col4Rows {
-				break
-			}
-
-			if board[col][row] == player &&
-				board[col+1][row+1] == player &&
-				board[col+2][row+2] == player &&
-				board[col+3][row+3] == player {
+			if board.Point(col, row) == player &&
+				board.Point(col+1, row+1) == player &&
+				board.Point(col+2, row+2) == player &&
+				board.Point(col+3, row+3) == player {
 				return true
 			}
 		}
 	}
 
+	// Check downslope diagonal
 	for col := 0; col <= cols-4; col++ {
-		col1Rows := len(board[col])
-		col2Rows := len(board[col+1])
-		col3Rows := len(board[col+2])
-		col4Rows := len(board[col+3])
-
-		if col1Rows <= 0 || col2Rows <= 0 || col3Rows <= 0 || col4Rows <= 0 {
-			continue
-		}
-
 		for row := 3; row < rows; row++ {
-			if row >= col1Rows || row-1 >= col2Rows || row-2 >= col3Rows || row-3 >= col4Rows {
-				break
-			}
-
-			if board[col][row] == player &&
-				board[col+1][row-1] == player &&
-				board[col+2][row-2] == player &&
-				board[col+3][row-3] == player {
+			if board.Point(col, row) == player &&
+				board.Point(col+1, row-1) == player &&
+				board.Point(col+2, row-2) == player &&
+				board.Point(col+3, row-3) == player {
 				return true
 			}
 		}
 	}
-
-	// Check diagonal (bottom-left to top-right)
-	// for row := 3; row < rows; row++ {
-	// 	for col := 0; col <= cols-4; col++ {
-	// 		if board[row][col] == player &&
-	// 			board[row-1][col+1] == player &&
-	// 			board[row-2][col+2] == player &&
-	// 			board[row-3][col+3] == player {
-	// 			return true
-	// 		}
-	// 	}
-	// }
 
 	return false
 }
