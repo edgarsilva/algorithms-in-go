148e3729cb20261574333000fa8b2b803c1ce605
edgarsilva <edgar.osc@gmail.com>
2024-09-26 12:02:24 -0600

    Trying new algo for tok k elements to comp vs ordering the slice.

--- top_k_elems_in_list/topelems.go
+++ top_k_elems_in_list/topelems.go
@@ -15,6 +15,7 @@ func TopKFrequent(nums []int, k int) []int {
 	for _, num := range nums {
 		if f, ok := m[num]; !ok {
 			m[num] = Freq{key: num, count: 1}
+			continue
 		} else {
 			f.count++
 			m[num] = f
@@ -29,11 +30,13 @@ func TopKFrequent(nums []int, k int) []int {
 	slices.SortFunc(sorted, func(a Freq, b Freq) int {
 		if a.count > b.count {
 			return -1
-		} else if a.count < b.count {
-			return +1
-		} else {
-			return 0
 		}
+
+		if a.count < b.count {
+			return +1
+		}
+
+		return 0
 	})
 
 	res := make([]int, 0, k)
@@ -42,4 +45,38 @@ func TopKFrequent(nums []int, k int) []int {
 	}
 
 	return res
+}
+
+// TopKFreqBucket using bucket sorted has bigger space but should have O time
+// perf, basically O(n) since we are not acteally sorting the slice
+func TopKFreqBucket(nums []int, k int) []int {
+	counter := make(map[int]Freq)
+
+	for _, num := range nums {
+		f, exists := counter[num]
+		if !exists {
+			counter[num] = Freq{key: num, count: 1}
+			continue
+		}
+
+		f.count++
+		counter[num] = f
+	}
+
+	bucket := make([][]int, len(nums)+1)
+	for k, c := range counter {
+		bucket[c.count] = append(bucket[c.count], k)
+	}
+
+	results := make([]int, 0, k)
+	for i := len(bucket) - 1; i > 0; i-- {
+		for _, n := range bucket[i] {
+			results = append(results, n)
+			if len(results) == k {
+				return results
+			}
+		}
+	}
+
+	return results
 }
--- top_k_elems_in_list/topelems_test.go
+++ top_k_elems_in_list/topelems_test.go
@@ -32,3 +32,52 @@ func TestTopKFrequent(t *testing.T) {
 		}
 	}
 }
+
+func TestTopKFreqBucket(t *testing.T) {
+	testcases := []struct {
+		nums []int
+		want []int
+		k    int
+	}{
+		{
+			nums: []int{1, 2, 2, 3, 3, 3},
+			k:    2,
+			want: []int{3, 2},
+		},
+		{
+			nums: []int{7, 7, 7, 2, 3, 4, 4, 5, 6, 6, 6, 6},
+			k:    3,
+			want: []int{6, 7, 4},
+		},
+		{
+			nums: []int{7, 7},
+			k:    1,
+			want: []int{7},
+		},
+	}
+
+	for _, tc := range testcases {
+		got := TopKFreqBucket(tc.nums, tc.k)
+		want := tc.want
+
+		if slices.Compare(got, want) != 0 {
+			t.Errorf("got %#v want %#v", got, want)
+		}
+	}
+}
+
+func BenchmarkFrequency(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		input := []int{7, 7, 7, 2, 3, 4, 4, 5, 6, 6, 6, 6}
+		k := 3
+		TopKFrequent(input, k)
+	}
+}
+
+func BenchmarkTopKFreqBucket(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		input := []int{7, 7, 7, 2, 3, 4, 4, 5, 6, 6, 6, 6}
+		k := 3
+		TopKFrequent(input, k)
+	}
+}
